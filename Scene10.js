43ef8aa54cb33ed0b16087336cb701bd8a476c63
MartinEier <81393982+MartinEier@users.noreply.github.com>
2021-07-20 10:02:01 -0300

    juego final

--- Scene10.js
+++ Scene10.js
@@ -13,7 +13,8 @@ class Scene10 extends Phaser.Scene {
       this.add.text(450, 350, 'Tienes 45 segundos para conseguir la gasolina', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
       this.add.text(410, 250, 'Haz logrado conseguir el tubo, se te atorgo una vida más', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
       this.add.text(455, 300, 'Ahora hay que cargar gasolina para la nave', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
-      var restartButton = this.add.text(620, 400, 'Siguiente nivel', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
+      this.add.text(415, 400, 'Cuidado, hay objetos que no se pueden agarrar, no te rindas', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
+      var restartButton = this.add.text(620, 450, 'Siguiente nivel', { fontFamily: 'Arial black', fontSize: 20, color: '#FF0000' })
       .setInteractive()
       .on('pointerdown', () => this.seguir() );
     }
